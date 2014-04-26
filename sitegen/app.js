9f6e5200cdc3bdf396d412c16f4335bafdffce06
root4me <pixelstripes@gmail.com>
2014-04-26 18:12:17 -0500

    updates

--- sitegen/app.js
+++ sitegen/app.js
@@ -1,62 +1,69 @@
-var markdown = require( "markdown" ).markdown;
+/*
+var markdown = require("markdown").markdown;
 var handlebars = require("handlebars");
 var fs = require('fs');
 
 var template = fs.readFileSync("./index.html", "utf8");
 
-var frontMatter = require('yaml-front-matter')
-  , results = frontMatter.loadFront('README', 'content');
-  
-  
-  console.log(results.content);
+var frontMatter = require('yaml-front-matter'),
+    results = frontMatter.loadFront('README', 'content');
 
-	var source = {
-	    title: results.title,
-		content : markdown.toHTML("" + results.content)
-	};
-	
-	var pageBuilder = handlebars.compile(template);
-	var pageText = pageBuilder(source);
+
+console.log(results.content);
+
+var source = {
+    title: results.title,
+    content: markdown.toHTML("" + results.content)
+};
+
+var pageBuilder = handlebars.compile(template);
+var pageText = pageBuilder(source);
 
 console.log(pageText);
-  
-  /*
-fs.readFile('README' , 'utf8' ,function (err, data) {
-  if (err) throw err;
-  
-  console.log(data);
-  
-	var source = {
-		message : markdown.toHTML("" + data)
-	};
-	
-	var pageBuilder = handlebars.compile(template);
-	var pageText = pageBuilder(source);
-	
-  
-  //console.log(markdown.toHTML("" + data));
-  
-  //console.log(pageText);
-  
-});
-
 */
 
-/*
-var handlebars = require("handlebars");
+// Read draft mark downs from draft folder
+var draftfolder = 'draft';
+var templateFolder = "template";
+var fs = require('fs');
+var path = require('path');
 
-var template = fs.readFileSync("./index.html", "utf8");
+var draftList = fs.readdirSync(draftfolder);
 
-function onRequest(req, res) {
-
-	var source = {
-		message : "hey handlebars"
-	};
-
-	var pageBuilder = handlebars.compile(template);
-	var pageText = pageBuilder(source);
-	res.writeHead(200, {"Context-Type": "text/html"});
-	res.write(pageText);
-	res.end();
+for (var i = 0; i < draftList.length; i++) {
+    console.log(path.join(draftfolder, draftList[i]));
+    generatehtml("post.html", path.join(draftfolder, draftList[i]))
 }
-*/
+
+
+// for each of the files convert markdown to html
+function generatehtml(templateName, markdownfile) {
+    var templateFile = path.join(templateFolder, templateName);
+    var handlebars = require("handlebars");
+    var frontMatter = require('yaml-front-matter');
+    var markdown = require("markdown").markdown;
+
+    var template = fs.readFileSync(templateFile, "utf8");
+
+    var results = frontMatter.loadFront(markdownfile, 'content');
+
+    var source = {
+        title: results.title,
+        content: markdown.toHTML(results.content)
+    };
+
+    var pageBuilder = handlebars.compile(template);
+    var pageText = pageBuilder(source);
+
+    console.log(pageText);
+
+}
+// read post template from templates folder
+
+// insert markdown converted to  html into the post template
+
+// create a folder based on the category specifed in markdown if it doesnt exist
+
+// create a file name based on the title with spaces stripped out
+
+// if file already exists, create a
