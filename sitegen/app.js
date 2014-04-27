d271bfce1e40eb0339e14295b75f779799a3d0ba
root4me <pixelstripes@gmail.com>
2014-04-27 13:31:26 -0500

    updates

--- sitegen/app.js
+++ sitegen/app.js
@@ -1,69 +1,66 @@
-/*
-var markdown = require("markdown").markdown;
-var handlebars = require("handlebars");
-var fs = require('fs');
-
-var template = fs.readFileSync("./index.html", "utf8");
-
-var frontMatter = require('yaml-front-matter'),
-    results = frontMatter.loadFront('README', 'content');
-
-
-console.log(results.content);
-
-var source = {
-    title: results.title,
-    content: markdown.toHTML("" + results.content)
-};
-
-var pageBuilder = handlebars.compile(template);
-var pageText = pageBuilder(source);
-
-console.log(pageText);
-*/
-
-// Read draft mark downs from draft folder
-var draftfolder = 'draft';
-var templateFolder = "template";
 var fs = require('fs');
 var path = require('path');
+var frontMatter = require('yaml-front-matter');
+var handlebars = require("handlebars")
+var markdown = require("markdown").markdown;
 
-var draftList = fs.readdirSync(draftfolder);
+// These can go into a config file at some point
+var draftFolder = 'draft';
+var templateFolder = "template";
+var publishFolder = "publish";
+
+
+var draftList = fs.readdirSync(draftFolder);
 
 for (var i = 0; i < draftList.length; i++) {
-    console.log(path.join(draftfolder, draftList[i]));
-    generatehtml("post.html", path.join(draftfolder, draftList[i]))
+
+    console.log(path.join(draftFolder, draftList[i]));
+    generatehtml("post.html",templateFolder,draftList[i],draftFolder);
 }
 
 
-// for each of the files convert markdown to html
-function generatehtml(templateName, markdownfile) {
-    var templateFile = path.join(templateFolder, templateName);
-    var handlebars = require("handlebars");
-    var frontMatter = require('yaml-front-matter');
-    var markdown = require("markdown").markdown;
+function isValidDraft(markdownfile) {
+    var article = frontMatter.loadFront(markdownfile, 'content');
+    if (article.title === undefined) return false;
+    return true;
+}
 
-    var template = fs.readFileSync(templateFile, "utf8");
+function generatehtml(templateName, templateFolder, draftFile, draftFolder) {
 
-    var results = frontMatter.loadFront(markdownfile, 'content');
+    var template = fs.readFileSync(path.join(templateFolder, templateName), 'utf8');
+    var article = frontMatter.loadFront(path.join(draftFolder, draftFile), 'content');
+    var publishFileName = null;
+
+    if (article.title !== undefined) {
+        // Replace all white spaces in the title with - 
+        publishFileName = path.join(publishFolder, article.title.replace(/\s+/g, '-') + ".html");
+    }
+    else {
+
+        console.log('-- Draft with out title can not be processed : ' + draftFile);
+
+        // No point in going further        
+        return;
+    }
 
     var source = {
-        title: results.title,
-        content: markdown.toHTML(results.content)
+        title: article.title,
+        author: article.author,
+        date: article.date,
+        content: markdown.toHTML(article.content)
     };
 
     var pageBuilder = handlebars.compile(template);
     var pageText = pageBuilder(source);
 
-    console.log(pageText);
+    // console.log(pageText);
+
+    if (publishFileName !== null) {
+        fs.writeFileSync(publishFileName, pageText, "utf8");
+
+        console.log('-- published : ' + publishFileName);
+        // Prepend the draft file with an _
+        fs.renameSync(path.join(draftFolder, draftFile), path.join(draftFolder, '_' + draftFile));
+    }
 
 }
-// read post template from templates folder
-
-// insert markdown converted to  html into the post template
-
-// create a folder based on the category specifed in markdown if it doesnt exist
-
-// create a file name based on the title with spaces stripped out
-
-// if file already exists, create a
