f75774b55f08c809953e8a9f962d28abf76567b0
root4me <pixelstripes@gmail.com>
2014-11-22 13:23:22 -0600

    updates to gruntfile and bower missing from last check in

--- site/Gruntfile.js
+++ site/Gruntfile.js
@@ -1,17 +1,20 @@
 module.exports = function(grunt) {
 
-    // Time how long tasks take. Can help when optimizing build times
+    // Time how long tasks take.
     require('time-grunt')(grunt);
 
     // Load grunt tasks automatically
     require('load-grunt-tasks')(grunt);
 
-    // Configurable paths
+    // Configurations
     var config = {
         app: 'app',
         dist: 'dist',
-        srcjs: ['../bower_components/foundation/js/vendor/modernizr.js', '../bower_components/foundation/js/vendor/jquery.js', '../bower_components/foundation/js/foundation/foundation.min.js'],
-        srccss: ['../bower_components/foundation/css/foundation.css']
+        srcjs: ['../bower_components/foundation/js/vendor/modernizr.js', '../bower_components/jquery/dist/jquery.js', 
+        '../bower_components/ScrollMagic/js/jquery.scrollmagic.js', '../bower_components/foundation/js/foundation/foundation.js',
+        '../bower_components/gsap/src/uncompressed/TweenMax.js', ],
+        srccss: ['../bower_components/foundation/css/foundation.css', 'css/scroll.css'],
+        srcimg: ['img/demo_tophat.png'],
     };
 
     // Define the configuration for all the tasks
@@ -28,8 +31,6 @@ module.exports = function(grunt) {
                     src: ['.tmp', '<%= config.dist %>/*', '!<%= config.dist %>/.git*']
                 }]
             },
-            server: '.tmp'
-            //??
         },
 
         processhtml: {
@@ -49,23 +50,28 @@ module.exports = function(grunt) {
                 files: [{
                     expand: true,
                     cwd: '<%= config.app %>',
-                    src: '<%= config.vendorjs %>',
-                    dest: 'dist/jstmp/*.*',
+                    src: '<%= config.srcimg %>',
+                    dest: '<%= config.dist %>',
 
                 }]
                 //console.log('test');
+
             },
 
         },
 
         uglify: {
             options: {
-                banner: '/*!<%=pkg.name%> <%=grunt.template.today("yyyy-mm-dd")%>*/\n'
+                banner: '/*Packaged on : <%=grunt.template.today("yyyy-mm-dd")%>*/\n'
             },
             dist: {
                 files: [{
                     expand: true,
                     flatten: true,
+                    extDot: 'last',
+                    /* Without this jquery.xyz.js and jquery.js at source will only produce one jquery.min.js at the destination folder
+                    Drove me nuts wondering why uglify seem to miss files. Turns out, i didnt read the doc close enough.
+                    But, why some one thought that setting extDot default to 'first' is the best thing to do is still a mystery. Extensions usually start after the last dot !  */
                     cwd: '<%= config.app %>',
                     src: '<%= config.srcjs %>',
                     dest: '<%= config.dist %>/js',
@@ -87,6 +93,6 @@ module.exports = function(grunt) {
         },
     });
 
-    grunt.registerTask('default', ['clean', 'processhtml', 'cssmin', 'uglify']);
+    grunt.registerTask('default', ['clean', 'copy:dist', 'processhtml', 'cssmin', 'uglify']);
 
 };
