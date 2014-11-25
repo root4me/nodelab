a07eb47299a0326e44c6cb0c2b002b5a05fe5c5b
root4me <pixelstripes@gmail.com>
2014-11-24 22:50:38 -0600

    processing logic to be Revamped

--- sitegen/app.js
+++ sitegen/app.js
@@ -4,7 +4,7 @@ var fileman = require('./fileman');
 var drafts = fileman.getDraftFiles('draft');
 
 var sortBycreateDate = function(list) {
-// Sort the payload newest create date first
+    // Sort the payload newest create date first
     list.sort(function(a, b) {
         if (Date.parse(a.createDate) > Date.parse(b.createDate)) return -1;
         if (Date.parse(a.createDate) < Date.parse(b.createDate)) return 1;
@@ -13,23 +13,34 @@ var sortBycreateDate = function(list) {
 }
 
 //Sort by ascending Id
-var sortById = function(list){
-        list.sort(function(a, b) {
+var sortById = function(list) {
+    list.sort(function(a, b) {
         if (parseFloat(a.draftId) > parseFloat(b.draftId)) return -1;
         if (parseFloat(a.draftId) < parseFloat(b.draftId)) return 1;
         if (parseFloat(a.draftId) == parseFloat(b.draftId)) return 0;
     });
 };
 
-var findById = function(list,id){
-    return list.filter(function(o){
+var findById = function(list, id) {
+    return list.filter = (function(o) {
         return o.draftId == "2";
     })
 }
 
 sortById(drafts);
 
-console.log(drafts);
+//console.log(drafts);
+
+for (var i = 0; i < drafts.length; i++) {
+    if (drafts.length > (i + 1)) {
+        drafts[i].prevPage = drafts[i + 1].publishFile;
+        drafts[i].prevPageTitle = drafts[i + 1].title;
+        drafts[i + 1].nextPage = drafts[i].publishFile;
+        drafts[i + 1].nextPageTitle = drafts[i].title;
+    }
+    console.log(drafts[i]);
+}
+
 
 /*
 drafts.forEach(function(draft) {
