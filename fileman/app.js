5d5a993ddb08bf72a891422cab757a86540b1441
root4me <pixelstripes@gmail.com>
2014-11-28 15:18:04 -0600

    updates

--- fileman/app.js
+++ fileman/app.js
@@ -7,57 +7,64 @@ var readcompare = 1;
 var generatefile = 0;
 
 if (generatefile == 1) {
-    f.generatesample('tmp/template.txt', 1000000);
+    //f.generatesample('tmp/template.txt', 1000000, 'tmp/sample1m.txt');
+    //f.generatesample('tmp/template.txt', 500000, 'tmp/sample500k.txt');
+    
+    // do this only after the above files exist
+    //f.saveidlist('tmp/sample1m.txt', 'tmp/1mIds.txt'); // thik existing data here 
+    //f.saveidlist('tmp/sample500k.txt', 'tmp/500kIds.txt'); // massaging the incoming data
 }
 
 if (readcompare == 1) {
-    f.readfile('tmp/srcIds1.txt', 0, 13, false, function(a) {
-        console.log('1 done : ' + a.length);
-        list1 = a;
-        console.log('list 1 : ' + list1.length);
-        readdone(1);
-    });
 
 
-    f.readfile('tmp/srcIds.txt', 0, 13, false, function(a) {
-        console.log('2 done');
-        list2 = a
-        console.log('list 2 : ' + list2.length);
-        readdone(2);
-    });
-
-    var dcount = 0;
-    var readdone = function(a) {
-        dcount++;
-        console.log('done : ' + dcount);
-
-        if (dcount == 2) {
-            //console.log(list1.length);
-            //console.log(list2.length);
-            diffLists(list2, list1);
-        }
-    };
+        f.readidlist('tmp/1mIds.txt', 0, 13, false, function(a) {
+            list1 = a;
+            console.log('list 1 for compare : ' + list1.length);
+            readdone(1);
+        });
 
 
-    // list1 is the larger list. Create a diff file with the itmes in list1 that doesnt exist in list2
-    var diffLists = function(list1, list2) {
-        console.log('inside diff : ' + list1.length);
-        for (var i = 0; i < list1.length; i++) {
-            //console.log(list1[i]);
-            var pos = -1;
+        f.readidlist('tmp/500kIds.txt', 0, 13, false, function(a) {
+            list2 = a
+            console.log('list 2 for compare : ' + list2.length);
+            readdone(2);
+        });
 
-            if (list2.indexOf(list1[i]) == -1) {
-                //                list2.splice(pos, 1);
-                console.log(list1[i]);
-                fs.appendFileSync('tmp/diff.txt', list1[i] + '\n');
+        var dcount = 0;
+        var readdone = function(a) {
+            dcount++;
+            console.log('done populating list ' + dcount);
+
+            if (dcount == 2) {
+                diffLists(list1, list2);
             }
-            else {
-                //  console.log('not found : ' + list1[i]);
-                //not found
+        };
+
+
+        // list1 is the larger list. Create a diff file with the itmes in list1 that doesnt exist in list2
+        var diffLists = function(list1, list2) {
+            console.log('Start doing diff between lists of ' + list1.length + ' and ' + list2.length);
+            var out = 'tmp/diff.txt';
+            console.log('Output : ' +  out);
+            
+            console.time('Time to generate ' + out);
+            for (var i = 0; i < list1.length; i++) {
+
+                var pos = -1;
+
+                if (list2.indexOf(list1[i]) == -1) {
+
+                    fs.appendFileSync(out, list1[i] + '\n');
+                }
+                else {
+                    //console.log('.');
+                }
             }
-        }
-        console.log('1 : ' + list1.length);
-        console.log('2 : ' + list2.length);
-        //        console.log(util.inspect(process.memoryUsage()));
-    };
+//            console.log('1 : ' + list1.length);
+//            console.log('2 : ' + list2.length);
+            console.timeEnd('Time to generate ' + out);
+        };
+
+
 }
--- fileman/fileman.js
+++ fileman/fileman.js
@@ -4,23 +4,12 @@ var fs = require('fs'),
 // file to read
 // characters start to finish to read from row, user -1 as finish for while row
 // save to disk or no
-module.exports.readfile = function(file, start, finish, save, callback) {
+module.exports.readidlist = function(file, start, finish, save, callback) {
     var lr = new ll(file),
         list = [];
-    //fs.unlinkSync('tmp/srcIds.txt');
-    var c = 0;
 
     lr.on('line', function(line) {
         list.push(line.substring(start, finish).trim());
-
-        /*
-        if ((c++ % 2) === 0) {
-            fs.appendFileSync('tmp/srcIds1.txt', line.substring(start, finish).trim() + '\n');
-        }
-        
-        fs.appendFileSync('tmp/srcIds.txt', line.substring(start, finish).trim() + '\n');
-*/
-
     });
 
     lr.on('end', function() {
@@ -28,20 +17,35 @@ module.exports.readfile = function(file, start, finish, save, callback) {
     });
 };
 
-module.exports.saveidlist = function(input, out, callback) {
+
+// extract ids from input file and write the out file
+module.exports.saveidlist = function(input, out) {
+    console.log('Extracting ids and saving id to : ' + out);
+    
+    console.time('Time to generate ' + out);    
     var lr = new ll(input);
+    if (fs.existsSync(out)) {
+        fs.unlinkSync(out);
+    }
     
-    fs.unlinkSync(out);
-    
+    lr.on('line', function(line) {
+        fs.appendFileSync(out, line.substring(0, 13).trim() + '\n');
+    });
+
+    lr.on('end', function() {
+        console.timeEnd('Time to generate ' + out);
+    });
+
 };
 
 // input template file and number of rows to generate
 // if there is a string '1000000000000', it will be replaced with incremental values indicating row Ids
-module.exports.generatesample = function(file, rows) {
-    var lr = new ll(file),
+module.exports.generatesample = function(template, rows, out) {
+    var lr = new ll(template),
         ln = '',
         id = 1000000000000;
 
+    console.log('Generating : ' + out);
     lr.on('line', function(line) {
         if (line.trim().length !== 0) {
             ln = line;
@@ -49,12 +53,15 @@ module.exports.generatesample = function(file, rows) {
     });
 
     lr.on('end', function() {
-        fs.unlinkSync('tmp/samplegen.txt');
-        console.time('Time to generate sample');
-        for (var i = 0; i < rows; i++) {
-            fs.appendFileSync('tmp/samplegen.txt', ln.replace('1000000000000', id++) + '\n');
+        if (fs.existsSync(out)) {
+            fs.unlinkSync(out);
         }
-        console.timeEnd('Time to generate sample');
+
+        console.time('Time to generate ' + out);
+        for (var i = 0; i < rows; i++) {
+            fs.appendFileSync(out, ln.replace('1000000000000', id++) + '\n');
+        }
+        console.timeEnd('Time to generate ' + out);
     });
 
 };
