3153d963e457408d1a04181980e312889ac8f79a
root4me <pixelstripes@gmail.com>
2015-03-29 14:33:08 -0500

    project to scan media files and categorize them

---
+++ avscan/app.js
@@ -0,0 +1,64 @@
+var Ffmpeg = require('fluent-ffmpeg');
+
+
+// recursively scan directory and look at each file 
+
+// figure out if the file is a video , audio or image
+
+// if the file is a conpressed file, snoop into the compressed file and see if there is media files inside that 
+
+// once the above can be done, expand the program to categorize and move files into catrgory based directories
+
+// for the unncategorizable media files, programatically set tags to catrgorize based on criteria
+
+var fs = require('fs');
+
+var search = function(dir) {
+  
+  if (!fs.existsSync(dir)) {
+    return console.log('dir doesnt exist : ' + dir);
+  }
+
+  console.log("dir -> " + dir);
+  
+  var files = fs.readdirSync(dir),
+    path;
+  
+  for (var i = 0; i < files.length; i++) {
+    path = dir + '/' + files[i];
+
+    if (fs.statSync(path).isDirectory()) {
+      search(path);
+    }
+    else
+    {
+      console.log("file -> " + path)
+    }
+
+  }
+};
+
+search(process.argv[2]);
+
+
+
+/*
+Ffmpeg.getAvailableFormats(function(err, formats) {
+  console.log('Available formats:');
+  console.dir(formats);
+});
+*/
+
+
+/*
+
+Ffmpeg.ffprobe('/home/harish/Downloads/Path to Follow.mp3', function(err, metadata) {
+    console.dir(metadata);
+});
+
+
+Ffmpeg.ffprobe('/home/harish/Downloads/Path to Follow.mp3',function(metadata, err) {
+  console.log(require('util').inspect(metadata, false, null));
+});
+
+*/
