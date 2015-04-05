a877c12f8576fdb34f2a15b19df7acf091e2b488
root4me <pixelstripes@gmail.com>
2015-04-05 17:57:21 -0500

    updates from testing

--- mediascan/mediascan.js
+++ mediascan/mediascan.js
@@ -2,50 +2,64 @@ var fs = require('fs');
 var path = require('path');
 
 
-var found = 0;
-
-var it = 0;
-var j = 0;
-var rc = 0;
+var f = 0; // number of files processed
+var d = 0; // number of directories processed (including the root directory passed in as commandline parameter)
+var ta = 0; // used to keep track of total assets processed (sub dir + files)
+var mf = 0; // media found
+var ad = 0;
+var im = 0;
+var vd = 0;
 
 // Recursively read contents of the directory
-var scan = function(dir, cb) {
+var scan = function(dir, callback) {
 
-    //it++;
-     rc++;
+    d++;
     if (!fs.existsSync(dir)) {
         console.log('Path doesnt exist : ' + dir);
     }
 
     var dc = fs.readdirSync(dir);
 
-    j = j + dc.length;
+    //console.log('dir -> ' + dir);
+
+    ta = ta + dc.length;
 
     for (var i = 0; i < dc.length; i++) {
 
         var p = path.resolve(dir, dc[i]);
+        try {
+            if (fs.statSync(p).isFile()) {
+                getfiletype(p, function(file, mime) {
+                    f++;
+                    if (mime != undefined) {
+                        if (mime.indexOf('audio') == 0 || mime.indexOf('video') == 0 || mime.indexOf('image') == 0) {
+                            console.log(file + " -> " + mime);
+                            mf++;
+                            if (mime.indexOf('audio') == 0) ad++;
+                            if (mime.indexOf('video') == 0) vd++;
+                            if (mime.indexOf('image') == 0) im++;
 
-        if (fs.statSync(p).isFile()) {
-            getfiletype(p, function(file, mime) {
-                it++;
-                if (mime != undefined) {
-                    if (mime.indexOf('audio') > -1 || mime.indexOf('video') > -1 || mime.indexOf('image') > -1) {
-                        console.log(file + " -> " + mime);
-                        found++;
-                        //console.log('found - ' + found);
+                        }
                     }
-                }
-                cb('d..');
-            })
+                    callback(null, file);
+                })
+            }
+            else if (fs.statSync(p).isDirectory()) {
+                scan(p, callback);
+            }
+            else {
+                console.log("not a file and not a directory .. wtf !")
+            }
         }
-        else if (fs.statSync(p).isDirectory()) {
-            scan(p, cb);
-        }
-        else {
-            console.log("not a file and not a directory .. wtf !")
+        catch (e) {
+            console.log(e);
+            // need to reduce on from total assets since this exception would have failes processing of one file or directory
+            // so far only time i have seen this happen is when statSync is called on a circular symbolic link
+            // ELOOP, too many symbolic links encountered 
+
+            ta--;
         }
     }
-
 }
 
 var mmm = require('mmmagic'),
@@ -64,17 +78,15 @@ var getfiletype = function(file, callback) {
 
 
 scan(process.argv[2], function(data) {
-        
     //console.log('i=' + it + ' - ' + 'j=' + j + ' - rc=' + rc);
-    
-    if (it == rc)
-    {
-        console.log('dir count -> ' + (rc -1));
-    }
-    
-    if (j == (it+ (rc -1))) {
-        console.log('dir count -> ' + (rc -1));
-        console.log('found media -> ' + found);
+
+    if (ta == (f + (d - 1))) {
+        console.log('dir count -> ' + (d - 1));
+        console.log('files count -> ' + f);
+        console.log('audio files -> ' + ad);
+        console.log('video files -> ' + vd);
+        console.log('image files -> ' + im);
+        console.log('Total media -> ' + mf);
     }
 
 });
