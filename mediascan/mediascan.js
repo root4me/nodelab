bae0d6a761eabea595d565090626e4cc16c4a888
root4me <pixelstripes@gmail.com>
2015-04-05 00:30:06 -0500

    call backs from recursion

--- mediascan/mediascan.js
+++ mediascan/mediascan.js
@@ -1,36 +1,49 @@
 var fs = require('fs');
 var path = require('path');
 
+
 var found = 0;
 
-// Recursively read contents of the directory
-var scan = function(dir) {
+var it = 0;
+var j = 0;
+var rc = 0;
 
+// Recursively read contents of the directory
+var scan = function(dir, cb) {
+
+    //it++;
+     rc++;
     if (!fs.existsSync(dir)) {
-        return console.log('Path doesnt exist : ' + dir);
+        console.log('Path doesnt exist : ' + dir);
     }
 
     var dc = fs.readdirSync(dir);
 
+    j = j + dc.length;
+
     for (var i = 0; i < dc.length; i++) {
+
         var p = path.resolve(dir, dc[i]);
 
         if (fs.statSync(p).isFile()) {
-            //console.log(p);
-            // find the file type using mmmagic
             getfiletype(p, function(file, mime) {
-                if (mime.indexOf('audio') > -1 || mime.indexOf('video') > -1 || mime.indexOf('image') > -1) {
-                    console.log(file + " -> " + mime);
+                it++;
+                if (mime != undefined) {
+                    if (mime.indexOf('audio') > -1 || mime.indexOf('video') > -1 || mime.indexOf('image') > -1) {
+                        console.log(file + " -> " + mime);
+                        found++;
+                        //console.log('found - ' + found);
+                    }
                 }
+                cb('d..');
             })
         }
         else if (fs.statSync(p).isDirectory()) {
-            scan(p);
+            scan(p, cb);
         }
         else {
             console.log("not a file and not a directory .. wtf !")
         }
-
     }
 
 }
@@ -47,9 +60,21 @@ var getfiletype = function(file, callback) {
         }
         callback(file, mime);
     });
-
 }
 
 
-scan(process.argv[2]);
+scan(process.argv[2], function(data) {
+        
+    //console.log('i=' + it + ' - ' + 'j=' + j + ' - rc=' + rc);
+    
+    if (it == rc)
+    {
+        console.log('dir count -> ' + (rc -1));
+    }
+    
+    if (j == (it+ (rc -1))) {
+        console.log('dir count -> ' + (rc -1));
+        console.log('found media -> ' + found);
+    }
 
+});
