3527ec9dc6750f596fec37f916272f07f8ec43b4
root4me <pixelstripes@gmail.com>
2015-04-11 14:45:17 -0500

    capability to play audio

--- mediascan/audio.js
+++ mediascan/audio.js
@@ -3,7 +3,7 @@ var ffmpeg = require('fluent-ffmpeg');
 // Add/update metadata to audio file
 // ffmpeg -i samples/Mumbai_Effect.mp3 -metadata title="mumbai.effect"  samples/mumbai.effect.mp3 -y
 
-
+/*
 ffmpeg('samples/Mumbai_Effect.mp3')
 .output('samples/mumbai.effect.mp3')
 .outputOptions('-metadata title=mumbai.effect')
@@ -14,17 +14,35 @@ ffmpeg('samples/Mumbai_Effect.mp3')
 }
 )
 .run();
-
-
-// play audio
-// ffmpeg -i samples/mumbai.effect.mp3 -f alsa hw:0
-/*
-ffmpeg({
-  source: 'samples/mumbai.effect.mp3',
-  // set a custom [winston](https://github.com/flatiron/winston) logging instance (optional, default null which will cause fluent-ffmpeg to spawn a winston console logger)
-  logger: null,
-})
-.output('samples/mumbai.effect.mp3')
-.outputOptions('-f alsa hw:0')
-.run();
 */
+
+// play audio for 20 seconds
+// ffmpeg -i samples/mumbai.effect.mp3 -f alsa hw:0
+
+var readline = require('readline'),
+    rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+
+rl.setPrompt('>', 1);
+
+var play = function(file, from, to) {
+
+    ffmpeg(file)
+        .seekInput(from)
+        .duration(to - from)
+        .output('hw:0')
+        .outputOptions(['-f', 'alsa'])
+        .on('end', function(err, data) {
+            if (err) {
+                console.log(err);
+            }
+            rl.question("Play some more (in seconds) : ", function(data) {
+                play(file,to,parseInt(to) + parseInt(data));
+            });
+        })
+        .run();
+}
+
+play('samples/mumbai.effect.mp3', 0, 10);
