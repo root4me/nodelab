66bfea77362daf196eb3e1680435f470358c5f94
root4me <pixelstripes@gmail.com>
2015-08-15 23:29:01 -0500

    redesigned site template

--- site/Gruntfile.js
+++ site/Gruntfile.js
@@ -10,7 +10,8 @@ module.exports = function(grunt) {
     var config = {
         app: 'app',
         dist: 'dist',
-        srcjs: ['js/*.*', '../bower_components/foundation/js/vendor/modernizr.js', '../bower_components/jquery/dist/jquery.js', '../bower_components/ScrollMagic/js/jquery.scrollmagic.js', '../bower_components/foundation/js/foundation/foundation.js', '../bower_components/gsap/src/uncompressed/TweenMax.js', ],
+        srcjs: ['js/*.*', '../bower_components/foundation/js/vendor/modernizr.js', '../bower_components/jquery/dist/jquery.js',
+        '../bower_components/foundation/js/foundation/foundation.js', '../bower_components/foundation/js/foundation/foundation.topbar.js'],
         srccss: ['css/*.*', '../bower_components/foundation/css/foundation.css'],
         srcimg: ['img/*.*'],
     };
